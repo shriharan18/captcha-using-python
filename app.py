bdc3a3ef0fafce7e3f384d0e05f730a8799ead23
Shriharan <74127448+tonyshree18@users.noreply.github.com>
2021-06-18 16:07:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+# import the package
+from captcha.image import ImageCaptcha
+
+# store it in image and set the dimensions
+image = ImageCaptcha(width=280, height=90)
+
+# generate method
+data = image.generate('7gb9hju58f')
+
+#write it into a file
+image.write('7gb9hju58f', 'captcha1.png')
